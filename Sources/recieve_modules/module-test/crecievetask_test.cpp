354ba890bfd81ded68472a8c49aacad3401b304b
Alexey Edelev <semlanik@aedelev.(none)>
2010-08-16 10:59:56 +0400

    Combining modules in one project

--- Sources/recieve_modules/module-test/crecievetask_test.cpp
+++ Sources/recieve_modules/module-test/crecievetask_test.cpp
@@ -5,8 +5,11 @@ CRecieveTask_test::CRecieveTask_test()
 
 }
 
-bool CRecieveTask_test::init(int maxThreads, CDataBaseHandler *, const siterules_ti &rule)
+bool CRecieveTask_test::init(int maxThreads, const siterules_ti &rule)
 {
+    Q_UNUSED(maxThreads)
+    Q_UNUSED(rule)
+    m_signaller = new CRecieveTaskSignaller;
     return true;
 }
 
@@ -17,13 +20,7 @@ bool CRecieveTask_test::run()
 }
 CRecieveTaskSignaller* CRecieveTask_test::signaller()
 {
-    m_signaller = new CRecieveTaskSignaller;
     return m_signaller;
-}
-
-CDataBaseHandler* CRecieveTask_test::database()
-{
-    return NULL;
 }
 
 Q_EXPORT_PLUGIN2(CRecieveTask_test, CRecieveTask_test);
--- Sources/recieve_modules/module-test/crecievetask_test.h
+++ Sources/recieve_modules/module-test/crecievetask_test.h
@@ -12,10 +12,9 @@ class CRecieveTask_test : public QObject, public CRecieveTask
     Q_INTERFACES(CRecieveTask)
 public:
     CRecieveTask_test();
-    virtual bool init(int maxThreads, CDataBaseHandler*, const siterules_ti& rule);
+    virtual bool init(int maxThreads, const siterules_ti& rule);
     virtual CRecieveTaskSignaller* signaller();
     virtual bool run();
-    virtual CDataBaseHandler* database();
 private:
     CRecieveTaskSignaller *m_signaller;
 };
