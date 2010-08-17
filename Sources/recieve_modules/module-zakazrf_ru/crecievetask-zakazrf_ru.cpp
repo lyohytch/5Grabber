850515fa66c3caa53da3f2a9464fb66dc3b9b869
Alexey Edelev <semlanik@gmail.com>
2010-08-18 01:16:12 +0400

    1

--- Sources/recieve_modules/module-zakazrf_ru/crecievetask-zakazrf_ru.cpp
+++ Sources/recieve_modules/module-zakazrf_ru/crecievetask-zakazrf_ru.cpp
@@ -111,7 +111,7 @@ void  CRecieveTask_zakazrf_ru::onThreadFinished()
 //        qDebug()<<__FILE__<<"("<<__LINE__<<") "<<Q_FUNC_INFO<<"isFinished"<<m_dataStructures.at(i)->isFinished();
         if(m_dataStructures.at(i)->isRoot() && m_dataStructures.at(i)->isFinished())
         {
-            qDebug()<<__FILE__<<"("<<__LINE__<<") "<<Q_FUNC_INFO;
+//            qDebug()<<__FILE__<<"("<<__LINE__<<") "<<Q_FUNC_INFO;
             m_signaller->onDataReady(m_dataStructures.value(i));
             continue;
         }
